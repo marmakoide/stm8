376914f3dd8a380f4a9ab5cbc243b635448ffbe1
Alexandre Devert <marmakoide@hotmail.fr>
2022-05-20 16:01:11 +0200

    Used loops to build the list of C source files to generate for stm8kit

--- stm8kit/autogen_files.mk
+++ stm8kit/autogen_files.mk
@@ -1,33 +1,5 @@
+GPIO_PIN_IDS = 0 1 2 3 4 5 6 7
+GPIO_FUNCTION_SUFFIXES = is_high set_as_output enable_pullup toggle
+
 GENERATED_SOURCES = \
-	./src/GPIO/gpio_pin_C0__is_high.c \
-	./src/GPIO/gpio_pin_C1__is_high.c \
-	./src/GPIO/gpio_pin_C2__is_high.c \
-	./src/GPIO/gpio_pin_C3__is_high.c \
-	./src/GPIO/gpio_pin_C4__is_high.c \
-	./src/GPIO/gpio_pin_C5__is_high.c \
-	./src/GPIO/gpio_pin_C6__is_high.c \
-	./src/GPIO/gpio_pin_C7__is_high.c \
-	./src/GPIO/gpio_pin_C0__set_as_output.c \
-	./src/GPIO/gpio_pin_C1__set_as_output.c \
-	./src/GPIO/gpio_pin_C2__set_as_output.c \
-	./src/GPIO/gpio_pin_C3__set_as_output.c \
-	./src/GPIO/gpio_pin_C4__set_as_output.c \
-	./src/GPIO/gpio_pin_C5__set_as_output.c \
-	./src/GPIO/gpio_pin_C6__set_as_output.c \
-	./src/GPIO/gpio_pin_C7__set_as_output.c \
-	./src/GPIO/gpio_pin_C0__enable_pullup.c \
-	./src/GPIO/gpio_pin_C1__enable_pullup.c \
-	./src/GPIO/gpio_pin_C2__enable_pullup.c \
-	./src/GPIO/gpio_pin_C3__enable_pullup.c \
-	./src/GPIO/gpio_pin_C4__enable_pullup.c \
-	./src/GPIO/gpio_pin_C5__enable_pullup.c \
-	./src/GPIO/gpio_pin_C6__enable_pullup.c \
-	./src/GPIO/gpio_pin_C7__enable_pullup.c \
-	./src/GPIO/gpio_pin_C0__toggle.c \
-	./src/GPIO/gpio_pin_C1__toggle.c \
-	./src/GPIO/gpio_pin_C2__toggle.c \
-	./src/GPIO/gpio_pin_C3__toggle.c \
-	./src/GPIO/gpio_pin_C4__toggle.c \
-	./src/GPIO/gpio_pin_C5__toggle.c \
-	./src/GPIO/gpio_pin_C6__toggle.c \
-	./src/GPIO/gpio_pin_C7__toggle.c
+	$(foreach suffix, $(GPIO_FUNCTION_SUFFIXES), $(foreach pin_id, $(GPIO_PIN_IDS), ./src/GPIO/gpio_pin_C$(pin_id)__$(suffix).c))
