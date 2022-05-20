ce3ab0bc7adf8c911512be7a1b8b5919c08b910d
Alexandre Devert <marmakoide@hotmail.fr>
2022-05-20 18:58:29 +0200

    Full coverage for GPIO implementation in stm8kit

--- stm8kit/autogen_files.mk
+++ stm8kit/autogen_files.mk
@@ -1,5 +1,7 @@
 GPIO_PIN_IDS = 0 1 2 3 4 5 6 7
-GPIO_FUNCTION_SUFFIXES = is_high set_as_output enable_pullup toggle
+GPIO_PORTS = A B C D E F G H I
+GPIO_FUNCTION_SUFFIXES = is_high is_low set_as_output set_as_input set_high set_low toggle enable_pullup disable_pullup enable_interrupt disable_interrupt
 
 GENERATED_SOURCES = \
-	$(foreach suffix, $(GPIO_FUNCTION_SUFFIXES), $(foreach pin_id, $(GPIO_PIN_IDS), ./src/GPIO/gpio_pin_C$(pin_id)__$(suffix).c))
+	$(foreach suffix, $(GPIO_FUNCTION_SUFFIXES), $(foreach port, $(GPIO_PORTS), $(foreach pin_id, $(GPIO_PIN_IDS), ./src/GPIO/gpio_pin_$(port)$(pin_id)__$(suffix).c)))
+
--- stm8kit/autogen_rules.mk
+++ stm8kit/autogen_rules.mk
@@ -3,4 +3,4 @@ $(2): $(1) ./include/stm8kit/GPIO.h
 	cat $$< | sed -e 's/PORT_ID/$(3)/' -e 's/PIN_ID/$(4)/' > $$@
 endef
 
-$(foreach suffix, $(GPIO_FUNCTION_SUFFIXES), $(foreach pin_id, $(GPIO_PIN_IDS), $(eval $(call GPIO_function_rule,./src/GPIO/gpio_pin_XX__$(suffix).c.template,./src/GPIO/gpio_pin_C$(pin_id)__$(suffix).c,C,$(pin_id)))))
+$(foreach suffix, $(GPIO_FUNCTION_SUFFIXES), $(foreach port, $(GPIO_PORTS), $(foreach pin_id, $(GPIO_PIN_IDS), $(eval $(call GPIO_function_rule,./src/GPIO/gpio_pin_XX__$(suffix).c.template,./src/GPIO/gpio_pin_$(port)$(pin_id)__$(suffix).c,$(port),$(pin_id))))))
