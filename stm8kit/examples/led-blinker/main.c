070debb1625b318a98a9e12dbd59870370dd97bb
Alexandre Devert <marmakoide@hotmail.fr>
2022-05-20 15:02:20 +0200

    Removed redudant #define

--- stm8kit/examples/led-blinker/main.c
+++ stm8kit/examples/led-blinker/main.c
@@ -1,5 +1,3 @@
-#define F_CPU 2400000UL
-
 #include <stm8kit/GPIO.h>
 #include <stm8kit/delays.h>
 
@@ -9,8 +7,8 @@ main() {
 	gpio_pin_C5__set_as_output();
 	gpio_pin_C5__enable_pullup();
     
-    while(1) {
-    	gpio_pin_C5__toggle();
-        delay_ms(1000);
-    }
+	while(1) {
+		gpio_pin_C5__toggle();
+		delay_ms(1000);
+	}
 }
