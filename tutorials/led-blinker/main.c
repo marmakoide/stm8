bb797bb7a13d0a8b1885849090c9f470456c6682
Alexandre Devert <marmakoide@hotmail.fr>
2022-06-03 14:01:54 +0200

    Minor refactoring

--- tutorials/led-blinker/main.c
+++ tutorials/led-blinker/main.c
@@ -1,33 +1,34 @@
 #include <stdint.h>
 
 
-#define F_CPU 2400000UL
-#define _SFR_(mem_addr)     (*(volatile uint8_t *)(0x5000 + (mem_addr)))
+#define F_CPU 2000000UL
+#define _SFR_(mem_addr)     (*(volatile uint8_t *)(mem_addr))
 
 
-// PORT C
-#define PC_ODR      _SFR_(0x0A)
-#define PC_IDR      _SFR_(0x0B)
-#define PC_DDR      _SFR_(0x0C)
-#define PC_CR1      _SFR_(0x0D)
-#define PC_CR2      _SFR_(0x0E)
+// GPIO port C
+#define PC_ODR       _SFR_(0x500a) // Port C data output latch register
+#define PC_IDR       _SFR_(0x500b) // Port C input pin value register
+#define PC_DDR       _SFR_(0x500c) // Port C data direction register
+#define PC_CR1       _SFR_(0x500d) // Port C control register 1
+#define PC_CR2       _SFR_(0x500e) // Port C control register 2
 
 #define LED_PIN     5
 
 
-static inline
-void delay_ms(uint16_t ms) {
-    uint32_t i;
-    for (i = 0; i < ((F_CPU / 18000UL) * ms); i++)
+static inline void
+delay_ms(uint16_t ms) {
+    for (uint32_t i = ((F_CPU / 18000UL) * ms); i != 0; --i)
         __asm__("nop");
 }
 
 
 void
 main() {
-    PC_DDR |= (1 << LED_PIN); // configure PC15 as output
-    PC_CR1 |= (1 << LED_PIN); // push-pull mode
-    
+	// Port C, pin 5 setup
+    PC_DDR |= (1 << LED_PIN); // Configure PC15 as output
+    PC_CR1 |= (1 << LED_PIN); // Push-pull mode
+
+	// Main loop    
     while (1) {
         PC_ODR ^= (1 << LED_PIN);
         delay_ms(1000);
